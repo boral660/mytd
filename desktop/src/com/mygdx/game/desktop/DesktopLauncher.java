7b7e85b5b9207d75d8365394da19d7ddf8142140
Alex-ПК\Alex <boral660@gmail.com>
2018-05-02 20:25:39 +0300

    Тест коммита с ошибкой

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.TDGame;
 
-public class DesktopLauncher {
+puыblic class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
                 config.title = "TDGame";
